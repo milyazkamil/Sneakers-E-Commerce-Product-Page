81bf109e9c21bb76ff156ecc9c618e3845007c7a
Milyaz Kamil <milyazkamil@gmail.com>
2023-09-12 10:29:44 +0300

    created components of svg's such as close, delete used on the site

---
+++ src/components/SvgComponents/MenuSvg.jsx
@@ -0,0 +1,17 @@
+const MenuSvg = () => {
+  return (
+    <svg
+      id="menu-svg"
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 24 24"
+      height="1.5em"
+      width="1.5em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M4 6H20V8H4zM4 11H20V13H4zM4 16H20V18H4z"></path>
+    </svg>
+  )
+}
+export default MenuSvg;
