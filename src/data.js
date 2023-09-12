6c3aacd3e5e03f0c18ea8804060fdebc8982ca62
Milyaz Kamil <milyazkamil@gmail.com>
2023-09-12 10:10:06 +0300

    created a data array containing image url and navigation information

---
+++ src/data.js
@@ -0,0 +1,51 @@
+import image1 from "./assets/images/image-product-1.jpg";
+import image2 from "./assets/images/image-product-2.jpg";
+import image3 from "./assets/images/image-product-3.jpg";
+import image4 from "./assets/images/image-product-4.jpg";
+
+export const images = [
+  {
+    id: 1,
+    url: image1,
+  },
+  {
+    id: 2,
+    url: image2,
+  },
+  {
+    id: 3,
+    url: image3,
+  },
+  {
+    id: 4,
+    url: image4,
+  },
+];
+
+export const navigation = [
+  {
+    id: 1,
+    href: 'collections',
+    title: 'Collections',
+  },
+  {
+    id: 2,
+    href: 'men',
+    title: 'Men',
+  },
+  {
+    id: 3,
+    href: 'women',
+    title: 'Women',
+  },
+  {
+    id: 4,
+    href: 'about',
+    title: 'About',
+  },
+  {
+    id: 5,
+    href: 'contact',
+    title: 'Contact',
+  }
+];
