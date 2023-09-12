a0b2166f9548fe32f2f27bb3fc5698f7f7181798
Milyaz Kamil <milyazkamil@gmail.com>
2023-09-12 10:50:49 +0300

    created component with product price information

---
+++ src/components/ProductPrice.jsx
@@ -0,0 +1,11 @@
+const ProductPrice = () => {
+  return (
+    <div id="product-price-container">
+      <p id="new-price">
+        $125.00 <span id="discount-rate">50%</span>
+      </p>
+      <span id="old-price">$250.00</span>
+    </div>
+  )
+}
+export default ProductPrice;
